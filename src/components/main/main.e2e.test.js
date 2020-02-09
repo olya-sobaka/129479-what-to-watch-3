45c7398d4f1dd6932ecf6015ce1b4d2d834ad26b
olya-sobaka <46378189+olya-sobaka@users.noreply.github.com>
2020-02-09 21:44:12 +0100

    удаляет непонятный артефакт

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Main from "./main.js";
-import { any } from "prop-types";
 
 const movie = {
   title: `Kiki's Delivery Service`,
