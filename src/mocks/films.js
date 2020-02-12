afdd65d7920fd6b0c14afe8735deb1475d1b1e60
olya-sobaka <46378189+olya-sobaka@users.noreply.github.com>
2020-02-12 21:29:39 +0100

    Создает моки для маленькой карточки фильма

---
+++ src/mocks/films.js
@@ -0,0 +1,36 @@
+const poster = `img/bohemian-rhapsody.jpg`;
+
+export const films = [
+  {
+    title: `Bohemian Rhapsody`,
+    src: poster
+  },
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    posterSrc: poster
+  },
+  {
+    title: `Macbeth`,
+    posterSrc: poster
+  },
+  {
+    title: `Aviator`,
+    posterSrc: poster
+  },
+  {
+    title: `Pulp Fiction`,
+    posterSrc: poster
+  },
+  {
+    title: `Moonrise Kingdom`,
+    posterSrc: poster
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    posterSrc: poster
+  },
+  {
+    title: `What We Do in the Shadows`,
+    posterSrc: poster
+  },
+];
