3d1fbc343405d45b0cc9d5760025fd3d1e636623
olya-sobaka <46378189+olya-sobaka@users.noreply.github.com>
2020-02-26 11:08:03 +0100

    обновляет моки

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,45 +1,63 @@
 const poster = `img/bohemian-rhapsody.jpg`;
+const previewMp4 = `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`;
+const previewWebm = `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`;
 
 const films = [
   {
     title: `Bohemian Rhapsody`,
     posterSrc: poster,
-    id: 1
+    id: 1,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Fantastic Beasts: The Crimes of Grindelwald`,
     posterSrc: poster,
-    id: 2
+    id: 2,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Macbeth`,
     posterSrc: poster,
-    id: 3
+    id: 3,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Aviator`,
     posterSrc: poster,
-    id: 4
+    id: 4,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Pulp Fiction`,
     posterSrc: poster,
-    id: 5
+    id: 5,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Moonrise Kingdom`,
     posterSrc: poster,
-    id: 6
+    id: 6,
+    previewMp4,
+    previewWebm
   },
   {
     title: `Bohemian Rhapsody`,
     posterSrc: poster,
-    id: 7
+    id: 7,
+    previewMp4,
+    previewWebm
   },
   {
     title: `What We Do in the Shadows`,
     posterSrc: poster,
-    id: 8
+    id: 8,
+    previewMp4,
+    previewWebm
   },
 ];
 
